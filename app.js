ebf327e261c4fa7d4cb3dde36d7c7cea2d700f18
CalebCraigo <CalebDOS1@yahoo.com>
2019-12-12 16:27:39 -0500

    streamline functions with callbacks

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const url = 'https://api.darksky.net/forecast/f522e1a771b36bfd8c9f3b4e01812377/37.8267,-122.4233'
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
 
-request({ url: url, json: true }, (error, response) => {
-    console.log(response.body.daily.data[0].summary + 'It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+geocode('Greenville, South Carolina', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
